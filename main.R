dc2cd984a1e63fef1631d93c4d2b1684ccf1c8ac
kiko1004 <kirilspiridonov@abv.bg>
2021-12-19 15:42:37 +0200

    basic analysis + first hypothesis

--- main.R
+++ main.R
@@ -1,2 +1,47 @@
+# Loading libraries -------------------
+
+library(dplyr)
+library(lubridate)
+
+# Loading data and transformation and basic overview --------------------------
+
+# Loading dataset
+
 dd <- read.csv('https://covid.ourworldindata.org/data/owid-covid-data.csv')
+lapply(dd, class)
+
+dd$date = ymd((dd$date))
+min(dd$date)
 max(dd$date)
+
+# Creating variable to get monthly data in further processes
+
+dd$is_new_month = floor_date(dd$date, 'month') == dd$date
+summary(dd)
+
+# Creating dataframe of columns, we have most observations of
+dd_f <- dd[, which(colMeans(!is.na(dd)) > 0.5)]
+
+# Areas of research --------------------------
+
+# Looking at the hypothesis that strict monetary policies help the spread of covid
+# Creating a data frame containing correlations between stringency index and total cases
+corr_stringency_total_cases = data.frame(Country = c("0"), correlation = c(0))
+
+countries = unique(dd_f$location)
+
+for (country in countries)
+{
+  dd_country = dd[dd$location == country,]
+  dd_country = dd_country[,colnames(dd_country) %in% c("location", "stringency_index", "total_cases")]
+  dd_country = na.omit(dd_country)
+  tryCatch( { correlation = cor(dd_country$stringency_index, dd_country$total_cases, use="complete.obs") }
+            , error = function(e) {correlation = NULL})
+  
+  df = data.frame(Country = c(country), correlation = c(correlation))
+  corr_stringency_total_cases = bind_rows(corr_stringency_total_cases,df)
+  
+}
+
+summary(corr_stringency_total_cases)
+
